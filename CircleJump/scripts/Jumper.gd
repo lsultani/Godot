53452d110c2fffb4bae473fc096942351f8b3c5b
Leslie Sultani <lesliesultani@gmail.com>
2019-06-27 20:38:50 -0700

    CircleJump - Part 1 Circle, Jumper and Main Scenes

---
+++ CircleJump/scripts/Jumper.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+var velocity = Vector2(100, 0)
+var jump_speed = 1000
+var target = null
+
+func _unhandled_input(event):
+	if target and event is InputEventScreenTouch and event.pressed:
+		jump()
+
+func jump():
+	target = null
+	# velocity = direction we are pointing X jump speed
+	velocity = transform.x * jump_speed
+
+func _on_Jumper_area_entered(area):
+	target = area
+	velocity = Vector2.ZERO
+
+func _physics_process(delta):
+	if target:
+		transform = target.orbit_position.global_transform
+	else:
+		position += velocity * delta
